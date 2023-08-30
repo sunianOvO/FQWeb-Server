f465f289e8204999d311f3cb4d37748c40370cb6
风月残烛 <61369261+fengyuecanzhu@users.noreply.github.com>
2023-08-30 08:04:53 +0800

    Update server.py

--- server.py
+++ server.py
@@ -218,11 +218,10 @@ def manage_domains():
             # Move domains from recycle bin back to node pool if they become accessible again
             for domain in recycle_bin:
                 if is_domain_accessible(domain):
-                    node_pool.append(domain)
+                    if not is_domain_exists(domain['domain']):
+                        node_pool.append(domain)
                     recycle_bin.remove(domain)
-                    if 'token' in domain and domain['token']:
-                        add_or_update_token(domain['token'], (10 + delta) * 3)
-
+                    
             # Remove domains from recycle bin if they are inaccessible for more than an hour
             for domain in recycle_bin:
                 if time.time() - domain['timestamp'] >= max_remove_time:
